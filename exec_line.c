a648e395363afff5cb474d449d58ef95127aa81a
GentlePhilip <aphilipnyarko@gmail.com>
2023-07-19 21:21:53 -0700

    Simple Shell

--- exec_line.c
+++ exec_line.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
+
 /**
  * exec_line - searches for commands and built-ins
  *
  * @datash: important info (args)
  * Return: 1 on success.
  */
+
 int exec_line(data_shell *datash)
 {
 	int (*builtin)(data_shell *datash);
