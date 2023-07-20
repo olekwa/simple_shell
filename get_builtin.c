60c59d369840cc2f40174f64da67c61ecd5801d8
GentlePhilip <aphilipnyarko@gmail.com>
2023-07-20 14:52:55 -0700

    Simple Shell

---
+++ get_builtin.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_builtin -built-in feature that carries out the command from the argument
+ * @cmd: command
+ * Return: the integrated command's function identification
+ */
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
